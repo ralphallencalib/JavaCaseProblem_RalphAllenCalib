64e85b4eaac61a49ed00e6afc3f11682c8fde1c2
ralphallencalib <calib.ralphallen40@gmail.com>
2025-10-17 13:59:00 +0800

    Uploaded Java case problem and output screenshot

---
+++ OOP-PIT/DivideCase.java
@@ -0,0 +1,13 @@
+public class DivideCase {
+    public static void main(String[] args) {
+        System.out.println("Name: Ralph Allen Calib");
+        System.out.println("ID Number: 2024304226");
+        System.out.println("----------------------------------");
+
+        double num1 = 100;  
+        double num2 = 20;  
+        double quotient = num1 / num2;
+
+        System.out.println("Result: The quotient of " + num1 + " and " + num2 + " is " + quotient);
+    }
+}
